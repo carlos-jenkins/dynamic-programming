0e28114317bd591782f683961def7d759613b506
Carlos Miguel Jenkins Pérez <carlos@jenkins.co.cr>
2012-09-20 03:09:16 -0600

    Digest report added to Floyd.

--- src/floyd/report.c
+++ src/floyd/report.c
@@ -31,14 +31,14 @@ bool floyd_report(floyd_context* c)
     if(!success) {
         return false;
     }
+    fprintf(report, "\n");
 
     /* Write header */
-    fprintf(report, "\n");
     fprintf(report, "\\section{%s}\n\n", "Floyd's algorithm");
     fprintf(report, "\\noindent{\\huge %s.} \\\\[0.4cm]\n",
                     "Dynamic programming");
     fprintf(report, "{\\LARGE %s.}\\\\[0.4cm]\n", "Operation Research");
-    fprintf(report, "\\HRule \\\\[0.4cm]\n");
+    fprintf(report, "\\HRule \\\\[0.5cm]\n");
 
     /* Write description */
     fprintf(report, "\\indent ");
@@ -48,7 +48,7 @@ bool floyd_report(floyd_context* c)
     }
 
     /* Write first section */
-    fprintf(report, "\\\\[0.7cm] \\noindent{\\Large Details:}\n");
+    fprintf(report, "\\\\[0.5cm]\n\\noindent{\\Large Details:}\n");
     fprintf(report, "\\begin{compactitem}\n");
     fprintf(report, "\\item %s : \\textsc{%s}. \n",
                     "Executed on", get_current_time());
@@ -74,6 +74,7 @@ bool floyd_report(floyd_context* c)
         fprintf(report, "ERROR: Graph image could not be generated.");
     }
 
+    /* TOC */
     fprintf(report, "\\newpage\n\\tableofcontents\n\\newpage\n");
 
     /* Write execution */
@@ -83,14 +84,21 @@ bool floyd_report(floyd_context* c)
     if(!success) {
         return false;
     }
-
-    /* Write interpretation */
     fprintf(report, "\n");
+
+    /* Write analisis */
+    int bumpier = -1;
+    int startb, endb;
+
+    float heavier = -1.0;
+    int starth, endh;
+
     fprintf(report, "\\subsection{%s}\n", "Analisis");
     int counter = 0;
     for(int starti = 0; starti < c->nodes; starti++) {
         for(int endi = 0; endi < c->nodes; endi++) {
 
+            /* Ignore same node paths */
             if(starti == endi) {
                 continue;
             }
@@ -100,13 +108,16 @@ bool floyd_report(floyd_context* c)
                 fprintf(report, "\\clearpage\n");
             }
 
+            /* Preamble */
             fprintf(report, "\\subsubsection{%s: %s $\\longrightarrow$ %s}\n",
                             "Analisis for path",
                             c->names[starti], c->names[endi]);
             fprintf(report, "\\begin{compactitem}\n");
-            fprintf(report, "\\item %s : {\\Large %s \\subscript{(%i)}"
+            fprintf(report, "\\item %s : {\\Large %s \\subscript{(%i)}}"
                             "$\\longrightarrow$ ", "Optimal path",
                             c->names[starti], starti + 1);
+
+            /* Follow route */
             int jumps = 1;
             int next = (int)c->table_p->data[starti][endi];
             while(next != 0) {
@@ -115,7 +126,9 @@ bool floyd_report(floyd_context* c)
                 next = (int)c->table_p->data[next - 1][endi];
                 jumps++;
             }
-            fprintf(report, "%s \\subscript{(%i)}}.\n",
+
+            /* Write subreport */
+            fprintf(report, "%s \\subscript{(%i)}.\n",
                             c->names[endi], endi + 1);
             fprintf(report, "\\item %s : {\\Large %i}.\n",
                             "Total jumps", jumps);
@@ -133,8 +146,54 @@ bool floyd_report(floyd_context* c)
                 }
             }
             fprintf(report, "\\end{compactitem}\n");
+            fprintf(report, "\n");
+
+            /* Compare this iteration */
+            if(jumps > bumpier) {
+                bumpier = jumps;
+                startb = starti;
+                endb = endi;
+            }
+            if(distance > heavier) {
+                heavier = distance;
+                starth = starti;
+                endh = endi;
+            }
         }
     }
+    fprintf(report, "\n");
+
+    /* Digest */
+    fprintf(report, "\\subsection{%s}\n", "Digest");
+    fprintf(report, "\\begin{compactitem}\n");
+    if(bumpier != -1) {
+        fprintf(report, "\\item %s : {\\Large %s \\subscript{(%i)} "
+                        "$\\longrightarrow$ %s \\subscript{(%i)}} "
+                        "with %i jumps.\n",
+                        "Bumpier path",
+                        c->names[startb], startb + 1,
+                        c->names[endb], endb + 1,
+                        bumpier);
+    }
+    if(heavier != -1.0) {
+        char* template = "\\item %s : {\\Large %s \\subscript{(%i)} "
+                         "$\\longrightarrow$ %s \\subscript{(%i)}} with a "
+                         "total distance of {\\Large %.4f}.\n";
+        if(floorf(heavier) == heavier) {
+            template = "\\item %s : {\\Large %s \\subscript{(%i)} "
+                       "$\\longrightarrow$ %s \\subscript{(%i)}} with a "
+                       "total distance of {\\Large %.0f}.\n";
+        } else if(heavier == PLUS_INF) {
+            template = "\\item %s : {\\Large %s \\subscript{(%i)} "
+                       "$\\longrightarrow$ %s \\subscript{(%i)}} with a "
+                       "total distance of {\\Large $\\infty$}.\n";
+        }
+        fprintf(report, template, "Longest path",
+                        c->names[starth], starth + 1,
+                        c->names[endh], endh + 1,
+                        heavier);
+    }
+    fprintf(report, "\\end{compactitem}\n");
     fprintf(report, "\n");
 
     /* End document */
