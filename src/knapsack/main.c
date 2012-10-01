f84cf91fde10ace4b5f91f6806a765255169780f
Carlos Miguel Jenkins Pérez <carlos@jenkins.co.cr>
2012-09-30 18:19:57 -0600

    Main logic for Knapsack implemented.

--- src/knapsack/main.c
+++ src/knapsack/main.c
@@ -23,10 +23,20 @@
 
 /* GUI */
 GtkWindow* window;
+GtkTreeView* items_view;
 GtkListStore* items_model;
 
 /* Context */
 knapsack_context* c = NULL;
+
+/* Functions */
+void add_row(GtkToolButton *toolbutton, gpointer user_data);
+void remove_row(GtkToolButton *toolbutton, gpointer user_data);
+void edit_started_cb(GtkCellRenderer* renderer, GtkCellEditable* editable,
+                     gchar* path, gpointer user_data);
+void cell_edited_cb(GtkCellRendererText* renderer, gchar* path,
+                    gchar* new_text, gpointer user_data);
+void process(GtkButton* button, gpointer user_data);
 
 int main(int argc, char **argv)
 {
@@ -51,10 +61,42 @@ int main(int argc, char **argv)
 
     /* Get pointers to objects */
     window = GTK_WINDOW(gtk_builder_get_object(builder, "window"));
+    items_view = GTK_TREE_VIEW(gtk_builder_get_object(builder, "items_view"));
     items_model = GTK_LIST_STORE(gtk_builder_get_object(builder, "items_model"));
+
+    /* Configure cell renderers callback */
+    GtkCellRenderer* name_renderer = GTK_CELL_RENDERER(
+                            gtk_builder_get_object(builder, "name_renderer"));
+    g_signal_connect(G_OBJECT(name_renderer),
+                         "edited", G_CALLBACK(cell_edited_cb),
+                         GINT_TO_POINTER(0));
+
+    GtkCellRenderer* value_renderer = GTK_CELL_RENDERER(
+                            gtk_builder_get_object(builder, "value_renderer"));
+    g_signal_connect(G_OBJECT(value_renderer),
+                         "edited", G_CALLBACK(cell_edited_cb),
+                         GINT_TO_POINTER(1));
+    edit_started_cb(value_renderer, NULL, NULL, NULL);
+
+    GtkCellRenderer* weight_renderer = GTK_CELL_RENDERER(
+                            gtk_builder_get_object(builder, "weight_renderer"));
+    g_signal_connect(G_OBJECT(weight_renderer),
+                         "edited", G_CALLBACK(cell_edited_cb),
+                         GINT_TO_POINTER(2));
+    edit_started_cb(weight_renderer, NULL, NULL, NULL);
+
+    GtkCellRenderer* amount_renderer = GTK_CELL_RENDERER(
+                            gtk_builder_get_object(builder, "amount_renderer"));
+    g_signal_connect(G_OBJECT(amount_renderer),
+                         "edited", G_CALLBACK(cell_edited_cb),
+                         GINT_TO_POINTER(3));
+    edit_started_cb(amount_renderer, NULL, NULL, NULL);
 
     /* Connect signals */
     gtk_builder_connect_signals(builder, NULL);
+
+    /* Initialize interface */
+    add_row(NULL, NULL);
 
     g_object_unref(G_OBJECT(builder));
     gtk_widget_show(GTK_WIDGET(window));
@@ -63,12 +105,113 @@ int main(int argc, char **argv)
     return(0);
 }
 
+void add_row(GtkToolButton *toolbutton, gpointer user_data)
+{
+    GtkTreeIter iter;
+    gtk_list_store_append(items_model, &iter);
+    gtk_list_store_set(items_model, &iter,
+                        0, "A",
+                        1, 1,
+                        2, 1,
+                        3, 1,
+                        -1);
+
+    GtkTreePath* model_path = gtk_tree_model_get_path(
+                                GTK_TREE_MODEL(items_model), &iter);
+    gtk_tree_view_set_cursor(items_view, model_path, NULL, false);
+    gtk_tree_path_free(model_path);
+
+    gtk_widget_grab_focus(GTK_WIDGET(items_view));
+    return;
+}
+
+void remove_row(GtkToolButton *toolbutton, gpointer user_data)
+{
+    int rows = gtk_tree_model_iter_n_children(
+                                    GTK_TREE_MODEL(items_model), NULL);
+    if(rows < 2) {
+        return;
+    }
+
+    GtkTreeSelection* selection = gtk_tree_view_get_selection(items_view);
+    GtkTreeIter iter;
+    if(gtk_tree_selection_get_selected(selection, NULL, &iter)) {
+
+        bool valid = gtk_list_store_remove(items_model, &iter);
+        if(!valid) {
+            valid = gtk_tree_model_iter_nth_child(GTK_TREE_MODEL(items_model),
+                                                  &iter, NULL, rows - 1);
+        }
+
+        if(valid) {
+            GtkTreePath* model_path = gtk_tree_model_get_path(
+                                        GTK_TREE_MODEL(items_model), &iter);
+            gtk_tree_view_set_cursor(items_view, model_path, NULL, false);
+            gtk_tree_path_free(model_path);
+        }
+    }
+}
+
+void edit_started_cb(GtkCellRenderer* renderer, GtkCellEditable* editable,
+                     gchar* path, gpointer user_data)
+{
+    GtkAdjustment* adj;
+
+    g_object_get(renderer, "adjustment", &adj, NULL);
+    if(adj) {
+        g_object_unref(adj);
+    }
+
+    adj = gtk_adjustment_new(
+                    1.00,           /* the initial value. */
+                    1.00,           /* the minimum value. */
+                    10000000.00,    /* the maximum value. */
+                    1.0,            /* the step increment. */
+                    10.0,           /* the page increment. */
+                    0.0             /* the page size. */
+                );
+    g_object_set(renderer, "adjustment", adj, NULL);
+}
+
+void cell_edited_cb(GtkCellRendererText* renderer, gchar* path,
+                    gchar* new_text, gpointer user_data)
+{
+    int row = atoi(path);
+    int column = GPOINTER_TO_INT(user_data);
+    printf("%s at (%i, %i)\n", new_text, row, column);
+
+    /* Get reference to model */
+    GtkTreePath* model_path = gtk_tree_path_new_from_string(path);
+    GtkTreeIter iter;
+    gtk_tree_model_get_iter(GTK_TREE_MODEL(items_model), &iter, model_path);
+    gtk_tree_path_free(model_path);
+
+    GValue value = G_VALUE_INIT;
+
+    if(column == 0) {
+        g_value_init(&value, G_TYPE_STRING);
+        g_value_set_string(&value, new_text);
+        gtk_list_store_set_value(items_model, &iter, column, &value);
+    } else {
+        g_value_init(&value, G_TYPE_INT);
+        g_value_set_int(&value, atoi(new_text));
+        gtk_list_store_set_value(items_model, &iter, column, &value);
+    }
+}
+
 void process(GtkButton* button, gpointer user_data)
 {
     if(c == NULL) {
         return;
     }
     knapsack_context_clear(c);
+
+    /*
+     * TODO: Create and fill a context:
+     *  1. Get capacity from widget and count number of item rows and
+     *     create context.
+     *  2. Fill context with items data.
+     */
 
     /* Execute algorithm */
     bool success = knapsack(c);
