d84ed7f34aab79bab0dc5db2cf4eee5628c89ab5
carolina <caro@ubuntu.(none)>
2012-10-03 01:40:11 -0600

    Knapsack save logic implemented

--- src/knapsack/main.c
+++ src/knapsack/main.c
@@ -406,9 +406,51 @@ void load_cb(GtkButton* button, gpointer user_data)
 void save(FILE* file)
 {
     printf("save()\n");
-    /**
-     * FIXME: IMPLEMENT
-     **/
+
+    fprintf( file, "%i\n",gtk_tree_model_iter_n_children(
+                                    GTK_TREE_MODEL(items_model), NULL));
+
+    GtkTreeIter iter;
+    bool was_set = gtk_tree_model_get_iter_first(
+                            GTK_TREE_MODEL(items_model), &iter);
+    if(!was_set) {
+        return;
+    }
+
+    GValue value = G_VALUE_INIT;
+
+    do {
+        gtk_tree_model_get_value(
+                            GTK_TREE_MODEL(items_model), &iter, 0, &value);
+        char* n = g_value_dup_string(&value);
+        g_value_unset(&value);
+
+        gtk_tree_model_get_value(
+                            GTK_TREE_MODEL(items_model), &iter, 1, &value);
+        int v = g_value_get_int(&value);
+        g_value_unset(&value);
+
+        gtk_tree_model_get_value(
+                            GTK_TREE_MODEL(items_model), &iter, 2, &value);
+        int w = g_value_get_int(&value);
+        g_value_unset(&value);
+
+        gtk_tree_model_get_value(
+                            GTK_TREE_MODEL(items_model), &iter, 3, &value);
+        int a = g_value_get_int(&value);
+        g_value_unset(&value);
+
+        was_set = gtk_tree_model_iter_next(
+                            GTK_TREE_MODEL(items_model), &iter);
+
+	fprintf(file, "%s %i %i %i\n", n, v, w, a);
+
+    } while(was_set);
+
+    fprintf(file, "%i\n",gtk_spin_button_get_value_as_int(capacity));
+    fprintf(file, "%s",  g_strdup(gtk_entry_get_text(unit)));
+    
+
 }
 
 void load(FILE* file)
